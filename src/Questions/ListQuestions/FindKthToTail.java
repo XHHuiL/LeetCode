8c1099172ceaee4084aaf706480c298cb2136484
liuhui <16302010048@fudan.edu.cn>
2019-12-19 17:00:13 +0800

    function: finish find kth node from tail to head

---
+++ src/Questions/ListQuestions/FindKthToTail.java
@@ -0,0 +1,37 @@
+package Questions.ListQuestions;
+
+import Structures.List.ListNode;
+
+public class FindKthToTail {
+
+    /*
+     * find the kth node from tail to head
+     * */
+    public ListNode findKthToTail(ListNode head, int k) {
+        // filter
+        if (head == null)
+            return null;
+        if (k <= 0)
+            return null;
+
+        ListNode p1 = head;
+        ListNode p2 = head;
+        // step 1: p1 move k-1 steps first
+        while (k > 1) {
+            if (p1.getNextNode() != null) {
+                p1 = p1.getNextNode();
+                k--;
+            }else
+                return null;
+        }
+
+        // step 2: move p2 to target node
+        while (p1.getNextNode() != null){
+            p1 = p1.getNextNode();
+            p2 = p2.getNextNode();
+        }
+
+        return p2;
+    }
+
+}
